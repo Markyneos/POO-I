cc86bcc2125a5488adefda14c9655e71a07545ad
Markyneos <mafaustine18@gmail.com>
2025-02-24 10:35:52 -0300

    Mudei uns métodos e uma classe

--- Aula20250213/src/main/java/com/mycompany/aula20250213/Main.java
+++ Aula20250213/src/main/java/com/mycompany/aula20250213/Main.java
@@ -3,29 +3,29 @@ package com.mycompany.aula20250213;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws ArithmeticException {
-        Scanner scanner = new Scanner(System.in);
         int input = 1;
         while (input != 0) {
             System.out.println("CALCULADORA");
             System.out.print("1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n0 - Sair\nEscolha: ");
             input = scanner.nextInt();
-            Operadores operacao = new Operadores();
             switch (input) {
                 case 1:
-                    System.out.println("Resposta: " + operacao.adicao() + "\n");
+                    System.out.println("Resposta: " + adicao() + "\n");
                     break;
                 case 2:
-                    System.out.println("Resposta: " + operacao.subtracao() + "\n");
+                    System.out.println("Resposta: " + subtracao() + "\n");
                     break;
                 case 3:
-                    System.out.println("Resposta: " + operacao.multiplicacao() + "\n");
+                    System.out.println("Resposta: " + multiplicacao() + "\n");
                     break;
                 case 4:
                     try {
-                        System.out.println("Resposta: " + operacao.divisao() + "\n");
+                        System.out.println("Resposta: " + divisao() + "\n");
                     } catch (ArithmeticException e) {
-                        System.err.println("Erro: não é possível dividir por 0");
+                        System.err.println("Erro: não é possível dividir por 0\n");
                     }
                     break;
                 case 0:
@@ -37,40 +37,72 @@ public class Main {
         }
         scanner.close();
     }
-}
 
-class Operadores {
-    Scanner scan = new Scanner(System.in);
-
-    public int adicao() {
+    public static int adicao() {
         System.out.print("Primeiro número: ");
-        int primeiro = scan.nextInt();
+        int primeiro = scanner.nextInt();
         System.out.print("Segundo: ");
-        int segundo = scan.nextInt();
+        int segundo = scanner.nextInt();
         return primeiro + segundo;
     }
 
-    public int subtracao() {
+    public static int subtracao() {
         System.out.print("Primeiro número: ");
-        int primeiro = scan.nextInt();
+        int primeiro = scanner.nextInt();
         System.out.print("Segundo: ");
-        int segundo = scan.nextInt();
+        int segundo = scanner.nextInt();
         return primeiro - segundo;
     }
 
-    public int multiplicacao() {
+    public static int multiplicacao() {
         System.out.print("Primeiro número: ");
-        int primeiro = scan.nextInt();
+        int primeiro = scanner.nextInt();
         System.out.print("Segundo: ");
-        int segundo = scan.nextInt();
+        int segundo = scanner.nextInt();
         return primeiro * segundo;
     }
 
-    public int divisao() {
+    public static int divisao() {
         System.out.print("Primeiro número: ");
-        int primeiro = scan.nextInt();
+        int primeiro = scanner.nextInt();
         System.out.print("Segundo: ");
-        int segundo = scan.nextInt();
+        int segundo = scanner.nextInt();
         return primeiro / segundo;
     }
 }
+
+// class Operadores {
+// Scanner scan = new Scanner(System.in);
+//
+// public int adicao() {
+// System.out.print("Primeiro número: ");
+// int primeiro = scan.nextInt();
+// System.out.print("Segundo: ");
+// int segundo = scan.nextInt();
+// return primeiro + segundo;
+// }
+//
+// public int subtracao() {
+// System.out.print("Primeiro número: ");
+// int primeiro = scan.nextInt();
+// System.out.print("Segundo: ");
+// int segundo = scan.nextInt();
+// return primeiro - segundo;
+// }
+//
+// public int multiplicacao() {
+// System.out.print("Primeiro número: ");
+// int primeiro = scan.nextInt();
+// System.out.print("Segundo: ");
+// int segundo = scan.nextInt();
+// return primeiro * segundo;
+// }
+//
+// public int divisao() {
+// System.out.print("Primeiro número: ");
+// int primeiro = scan.nextInt();
+// System.out.print("Segundo: ");
+// int segundo = scan.nextInt();
+// return primeiro / segundo;
+// }
+// }
